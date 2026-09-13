5c89c835eae70f7998749a612ec107fc80ea4d49
hemanth3007 <hemanthkrishna1912@gmail.com>
2026-09-13 21:14:20 +0530

    Added API Gateway Terraform configuration

---
+++ infrastructure/terraform/api_gateway.tf
@@ -0,0 +1,52 @@
+# API Gateway HTTP API
+resource "aws_apigatewayv2_api" "upload" {
+  name          = "cloudvision_upload_api"
+  protocol_type = "HTTP"
+  cors_configuration {
+    allow_credentials = false
+    allow_headers = [
+      "content-type"
+    ]
+    allow_methods = [
+      "POST",
+      "OPTIONS"
+    ]
+    allow_origins = [
+      "http://127.0.0.1:5500"
+    ]
+    max_age = 0
+  }
+}
+
+# API Gateway → Upload Lambda integration
+resource "aws_apigatewayv2_integration" "upload" {
+  api_id                 = aws_apigatewayv2_api.upload.id
+  integration_type       = "AWS_PROXY"
+  integration_method     = "POST"
+  integration_uri        = "arn:aws:lambda:ap-south-1:056641105958:function:cloudvision-upload-image"
+  payload_format_version = "2.0"
+  timeout_milliseconds   = 30000
+}
+
+# POST /upload route
+resource "aws_apigatewayv2_route" "upload" {
+  api_id    = aws_apigatewayv2_api.upload.id
+  route_key = "POST /upload"
+  target    = "integrations/${aws_apigatewayv2_integration.upload.id}"
+}
+
+# Default stage
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.upload.id
+  name        = "$default"
+  auto_deploy = true
+}
+
+# Allow API Gateway to invoke Upload Lambda
+resource "aws_lambda_permission" "api_gateway_upload" {
+  statement_id  = "e42abde1-fd93-5bf4-a102-ae64a8e19b8e"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upload.function_name
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.upload.execution_arn}/*/*/upload"
+}
