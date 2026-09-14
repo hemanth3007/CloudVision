967aeb1c7a29c9fd43ae4acdd011a1046fd36e57
hemanth3007 <hemanthkrishna1912@gmail.com>
2026-09-14 21:12:59 +0530

    Added IAM permissions for GitHub actions and auto frontend deployment function using yaml file

--- infrastructure/terraform/iam.tf
+++ infrastructure/terraform/iam.tf
@@ -155,3 +155,76 @@ resource "aws_iam_role_policy" "upload_dynamodb" {
     ]
   })
 }
+
+# GitHub Actions - OIDC provider
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+# GitHub Actions - IAM role
+resource "aws_iam_role" "github_actions" {
+  name = "CloudVision-GitHubActions-Role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+          }
+          StringLike = {
+            "token.actions.githubusercontent.com:sub" = "repo:hemanth3007/CloudVision:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+}
+
+# GitHub Actions - Frontend deployment permissions
+resource "aws_iam_role_policy" "github_actions_deploy" {
+  name = "CloudVision-GitHubActions-Deploy"
+  role = aws_iam_role.github_actions.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:ListBucket"
+        ]
+        Resource = "arn:aws:s3:::cloudvision-frontend-hk2005"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "s3:PutObject",
+          "s3:DeleteObject"
+        ]
+        Resource = "arn:aws:s3:::cloudvision-frontend-hk2005/*"
+      },
+      {
+        Effect = "Allow"
+        Action = [
+          "cloudfront:CreateInvalidation"
+        ]
+        Resource = "arn:aws:cloudfront::056641105958:distribution/E136IR91GKFPST"
+      }
+    ]
+  })
+}
